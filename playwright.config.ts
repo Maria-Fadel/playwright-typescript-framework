b66e3ad03d5e36779f65569fb035b49b7022f956
Maria-Fael <ichbinkieler@gmail.com>
2026-04-09 20:48:41 +0200

    add allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
    reporter: [
+    ['list'],     
     ['allure-playwright']
   ],
   /* Run tests in files in parallel */
